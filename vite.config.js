f18c7fa20889515cb443552899a85ac9145fc175
yibopeng <284261055@qq.com>
2026-01-19 17:41:47 +0800

    return to npx serve

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 如果有别名配置，可按需补充；没有就保持最简
 export default defineConfig({
-  base: '/admin_system_demo/',   // 关键：部署子路径
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true
+  }
 })
 
